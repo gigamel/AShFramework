914f911e538b92bce0a4edea9713c232d3f9b360
Aleksey Shapovalov <gigamel@yandex.ru>
2020-10-03 11:16:48 +0300

    Code formatting

--- autoload.php
+++ autoload.php
@@ -1,5 +1,5 @@
 <?php
 
 spl_autoload_register(function($class) {
-	// autoloader
+    // autoloader
 });
--- structures/Base.php
+++ structures/Base.php
@@ -4,22 +4,22 @@ namespace Structures;
 
 abstract class Base
 {
-	/**
-	 * @param string $name
-	 * @param array $args
-	 * @exceptions BadMethodCallException
-	 */
-	public function __call($name, $args)
-	{
-		throw new BadMethodCallException;
-	}
+    /**
+     * @param string $name
+     * @param array $args
+     * @exceptions BadMethodCallException
+     */
+    public function __call($name, $args)
+    {
+        throw new BadMethodCallException;
+    }
 	
-	/**
-	 * @param string $name
-	 * @exceptions UnexpectedValueException
-	 */
-	public function __get($name)
-	{
-		throw new UnexpectedValueException;
-	}
+    /**
+     * @param string $name
+     * @exceptions UnexpectedValueException
+     */
+    public function __get($name)
+    {
+        throw new UnexpectedValueException;
+    }
 }
